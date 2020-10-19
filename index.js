96a908ef3c1802b13e00357f214d11f3377cea0f
Tokhtar <nekorytaylor2@gmail.com>
2020-10-19 23:53:32 +0600

    loading browser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll({
-	el: document.querySelector('[data-scroll-container]'),
-	smooth: true,
-	smoothMobile: true,
-});
+document.addEventListener(
+	'DOMContentLoaded',
+	function () {
+		// your code goes here
+		const scroll = new LocomotiveScroll({
+			el: document.querySelector('[data-scroll-container]'),
+			smooth: true,
+			smoothMobile: true,
+		});
+	},
+	false
+);
